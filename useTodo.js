ee5b9965b808c3993396f1a1b272bbb175c935b7
iamjohangalvan <iamjohangalvan@gmail.com>
2025-07-11 16:35:09 -0400

    first commit

---
+++ useTodo.js
@@ -0,0 +1,47 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodo = (initialState = []) => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+    dispatch(action);
+  };
+
+  const handleRemoveTodo = (id) => {
+    const action = {
+      type: "[TODO] Remove Todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  return {
+    todos,
+    handleNewTodo,
+    handleRemoveTodo,
+    handleToggleTodo,
+    todosCount: todos.length,
+    pendingCount: todos.filter((todo) => !todo.done).length,
+  };
+};
